77dd5f169a5f7bece0cba28157cce833e71cd571
Alain Euksuzian <alain-mokour@hotmail.com>
2024-01-06 18:04:25 -0500

    solved more assignment

--- Assignment3/Ticketbooth.java
+++ Assignment3/Ticketbooth.java
@@ -9,12 +9,16 @@ public class Ticketbooth {
         this.busCards = null;
     }
 
+    public Ticket getTickets() {
+        return tickets;
+    }
+
     Ticketbooth(Ticket ticket, OPUSCard[] cards){
 
         this.tickets = new Ticket(ticket);
         this.busCards = new OPUSCard[cards.length];
 
-        for (int i =0; i < busCards.length; i++){
+        for (int i = 0; i < busCards.length; i++){
             busCards[i] = new OPUSCard(cards[i]);
         }
 
@@ -36,7 +40,116 @@ public class Ticketbooth {
     }
 
     public int getNumberOpusCardsBooth(){
+        if (busCards == null){
+            return 0;
+        }
         return busCards.length;
     }
 
+    public int addNewOpusCard(OPUSCard cardToAdd){
+        if (busCards == null){
+            busCards = new OPUSCard[1];
+            busCards[0] = cardToAdd;
+        }
+        else {
+            OPUSCard[] copyArray = new OPUSCard[busCards.length + 1];
+            for (int i = 0; i < busCards.length; i++ ){
+                copyArray[i] = busCards[i];
+            }
+            copyArray[copyArray.length - 1] = cardToAdd;
+            busCards = copyArray;
+        }
+        return busCards.length;
+
+    }
+
+    public boolean removeOpusCard(OPUSCard cardToRemove){
+       if (busCards == null){
+        return false;
+       }
+       else {
+        int index = 0;
+        int counter = 0;
+
+        for (OPUSCard element: busCards ){
+            if (element.equals(cardToRemove)){
+                index = counter;
+                break;
+            }
+            counter++;
+        }
+        
+        for (int i = index; i < busCards.length-1; i++ ){
+            busCards[i] = busCards[i + 1];
+        }
+        OPUSCard[] copyArray = new OPUSCard[busCards.length - 1];
+
+        for (int i =0; i < copyArray.length; i++ ){
+            copyArray[i] = busCards[i];
+        }
+        busCards = copyArray;
+        return true;
+       }
+    }
+
+    public void updateExpMonthYear(int newMonth, int newYear, OPUSCard cardToUpdate){
+        cardToUpdate.setExpiryMonth(newMonth);
+        cardToUpdate.setExpiryYear(newYear);
+    }
+
+
+    public int addTicketToBooth(int regular, int junior, int senior, int daily, int weekly){
+        tickets.addTickets(regular, junior, senior, daily, weekly);
+        return getTotalTicketsBooth();
+
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null){
+            return false;
+        }
+        else {
+            if (!(obj instanceof Ticketbooth)){
+               return false;
+            }
+            else{
+                 Ticketbooth otherObj = (Ticketbooth) obj;
+                return this.busCards.length == otherObj.busCards.length;
+            }
+
+        }
+    }
+
+    @Override
+    public String toString() {
+
+        int[] cardNumbers = {tickets.getRegularTickets(),tickets.getJuniorTickets(),tickets.getSeniorTickets(),
+             tickets.getDailyTickets(),tickets.getWeeklyTickets()};
+
+        String[] description = {"the number of regular tickets: ", "the number of junior tickets: ",
+        "the number of senior tickets: ", "the number of daily tickets: ", "the number of weekly tickets: " };
+        
+        final String NOCARD = " No OPUS cards";
+
+        String outputTicketNumbers = "";
+
+        for (int i = 0; i < cardNumbers.length; i++){
+            if (cardNumbers[i] > 0){
+                outputTicketNumbers += description[i] + cardNumbers[i];
+            }
+            else {
+                outputTicketNumbers += description[i] + NOCARD;
+            }
+        }
+
+        outputTicketNumbers+="\n";
+
+        for (OPUSCard e: busCards){
+            outputTicketNumbers += e.toString() + "\n";
+           
+        } 
+        return outputTicketNumbers;
+    }
 }
--- Assignment3/testIt.java
+++ Assignment3/testIt.java
@@ -3,21 +3,44 @@ package comp248.Assignment3;
 public class testIt {
     public static void main(String[] args) {
         OPUSCard card = new OPUSCard("asdf", "alain", 10, 2024);
-        OPUSCard card2 = new OPUSCard("asdaaf", "alsain", 11, 2245);
-        OPUSCard card3 = new OPUSCard("asaadf", "aladin", 11, 2028);
+        OPUSCard card2 = new OPUSCard("REMOVE", "REMOVE", 11, 2245);
+        OPUSCard card3 = new OPUSCard("asaadf", "jack", 11, 2028);
+        OPUSCard card4 = new OPUSCard("aaaa", "adss", 11, 2028);
 
-        OPUSCard[] allCards = {card,card2,card3};
+        int[] cardNumbers = {0,1,2,0,5};
 
-        for (OPUSCard e :allCards){
-            System.out.println(e);
-            System.out.println("__________________________________");
+       String[] description = {"the number of regular tickets: ", "the number of junior tickets: ",
+       "the number of senior tickets: ", "the number of daily tickets: ", "the number of weekly tickets: " };
+       final String NOCARD = " No OPUS cards";
+
+       String outputTicketNumbers = "";
+
+        for (int i = 0; i < cardNumbers.length; i++){
+            if (cardNumbers[i] > 0){
+                outputTicketNumbers += description[i] + cardNumbers[i] + "\n";
+            }
+            else {
+                outputTicketNumbers += description[i] + NOCARD + "\n";
+            }
         }
 
-       Ticketbooth myboot = new Ticketbooth();
-
-
+        System.out.println(outputTicketNumbers);
     }
 
+
+
+   /* public static void gs(){
+                OPUSCard[] busCards = {card,card2,card3, card4};
+        String output = "";
+        for (OPUSCard e: busCards){
+            output +="Cardholder type: " + e.getCardType() + "\nCardholder Name: " 
+            + e.getCardHolderName() + "\nexipiry date: " + e.getExpiryMonth() + "\nexpiry year: " + e.getExpiryYear() + "\n-------------------------\n";
+           
+        } */
+     
+    }
+
+
     
     
-}
+
